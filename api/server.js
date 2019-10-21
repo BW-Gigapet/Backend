ea7cd1edbb82fec2971f881957f9fc3ab589b3a7
danRolon <daniel.rolon@yahoo.com>
2019-10-21 13:56:09 -0500

    commenting out code to hopefully fix heroku issues

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const authRouter = require('../auth/authRouter.js');
 const userRouter = require('../users/userRouter.js');
 const childRouter = require('../child/childRouter.js');
-const mealRouter = require('../meals/mealRouter.js');
+//const mealRouter = require('../meals/mealRouter.js');
 
 const server = express();
 
@@ -16,6 +16,6 @@ server.use(cors());
 server.use('/api', authRouter);
 server.use('/api/users', userRouter);
 server.use('/api/child', childRouter);
-server.use('/api/meals', mealRouter);
+//server.use('/api/meals', mealRouter);
 
 module.exports = server;
